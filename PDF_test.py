a9266137d7a675eaa8c6d9e5da04cd946d5be083
simonedamiani <114066103+simonedamiani@users.noreply.github.com>
2022-10-13 17:20:35 +0200

    Initial commit

---
+++ PDF_test.py
@@ -0,0 +1,8 @@
+import unittest
+import sys
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+if sys.flags.interactive:
+    plt.ion()
--- assignment_3.py
+++ assignment_3.py
@@ -1,1 +1,7 @@
 """Third assignment of CMEPDA Course, 2022/2023"""
+
+import numpy as np
+from scipy.interpolate import InterpolatedUnivariateSpline
+import matplotlib.pyplot as plt
+
+
