24cb141a50b43565be0545ea28ba447b39d079c0
simonedamiani <114066103+simonedamiani@users.noreply.github.com>
2022-10-14 12:40:31 +0200

    Initial commit

--- PDF_test.py
+++ PDF_test.py
@@ -6,3 +6,63 @@ import matplotlib.pyplot as plt
 
 if sys.flags.interactive:
     plt.ion()
+
+from assignment_3 import ProbabilityDensityFunction
+
+
+class TestPDF(unittest.TestCase):
+    def test_uniform(self):
+        """Test a uniform distribution from class ProbabilityDensityFunction"""
+
+        x = np.linspace(0., 1., 100)
+        y = np.full(x.shape, 1.)
+        pdf = ProbabilityDensityFunction(x, y)
+        self.assertAlmostEqual(pdf.integral(0., 1.), 1.)
+        plt.figure('Uniform pdf')
+        plt.plot(x, pdf(x))
+        plt.figure('Uniform cdf')
+        plt.plot(x, pdf.cdf(x))
+        plt.figure('Uniform ppf')
+        plt.plot(x, pdf.ppf(x))
+        r = pdf.gen_random(1000000)
+        plt.figure('Uniform random variate')
+        plt.hist(r, bins=100)
+
+    def test_triangular(self):
+        """Test a triangular distribution from class ProbabilityDensityFunction"""
+
+        x = np.linspace(0., 1., 100)
+        y = 4. * x
+        pdf = ProbabilityDensityFunction(x, y)
+        self.assertAlmostEqual(pdf.integral(0., 1.), 1.)
+        plt.figure('Triangular pdf')
+        plt.plot(x, pdf(x))
+        plt.figure('Triangular cdf')
+        plt.plot(x, pdf.cdf(x))
+        plt.figure('Triangular ppf')
+        plt.plot(x, pdf.ppf(x))
+        r = pdf.gen_random(1000000)
+        plt.figure('Triangular random variate')
+        plt.hist(r, bins=100)
+
+    def test_exponential(self):
+        """Test an exponential distribution from class ProbabilityDensityFunction"""
+
+        x = np.linspace(0., 1., 100)
+        y = np.exp(10*x)
+        pdf = ProbabilityDensityFunction(x, y)
+        print(pdf.integral(0., 1.))
+        self.assertAlmostEqual(pdf.integral(0., 1.), 1.)
+        plt.figure('Exponential pdf')
+        plt.plot(x, pdf(x))
+        plt.figure('Exponential cdf')
+        plt.plot(x, pdf.cdf(x))
+        plt.figure('Exponential ppf')
+        plt.plot(x, pdf.ppf(x))
+        r = pdf.gen_random(1000000)
+        plt.figure('Exponential random variate')
+        plt.hist(r, bins=100)
+
+
+if __name__ == '__main__':
+    unittest.main(exit=not sys.flags.interactive)
--- assignment_3.py
+++ assignment_3.py
@@ -2,6 +2,31 @@
 
 import numpy as np
 from scipy.interpolate import InterpolatedUnivariateSpline
-import matplotlib.pyplot as plt
 
 
+class ProbabilityDensityFunction(InterpolatedUnivariateSpline):
+    """Class describing a probability density function"""
+
+    def __init__(self, x, y, k=3):
+        """Constructor of the probability density function"""
+        # Create and normalize (if it's not) the pdf
+        normalization = InterpolatedUnivariateSpline(x, y, k=k).integral(x[0], x[-1])
+        y /= normalization
+        InterpolatedUnivariateSpline.__init__(self, x, y, k=k)
+
+        # Create the cdf
+        y_cdf = np.array([self.integral(x[0], x_cdf) for x_cdf in x])
+        self.cdf = InterpolatedUnivariateSpline(x, y_cdf, k=k)
+
+        # Create the ppf
+        x_ppf, index_ppf = np.unique(y_cdf, return_index=True)
+        y_ppf = x[index_ppf]
+        self.ppf = InterpolatedUnivariateSpline(x_ppf, y_ppf, k=k)
+
+    def probability(self, x_1, x_2):
+        """Return the probability evaluated using the CDF distribution"""
+        return self.cdf(x_2) - self.cdf(x_1)
+
+    def gen_random(self, size=1000):
+        """Return an array of random value generated according to the chosen PDF"""
+        return self.ppf(np.random.uniform(size=size))
